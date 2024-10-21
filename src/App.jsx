00ceaa4eaf4925029b2998c535df9576cf636db0
Ashiqur1819 <ashiqurrahman1819@gmail.com>
2024-10-21 18:33:43 +0600

    Done primary setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div>      
+      <h1 className='bg-purple-400 text-teal-950 text-7xl text-center font-bold'>Vite + React</h1>   
+    </div>
+  )
+}
+
+export default App
